9fef84a9474dadc2cebed6d5b36dc5d947431597
Fex97 <felix-ericsson@live.se>
2019-04-05 10:00:45 +0100

    Added files

---
+++ serialTest.py
@@ -0,0 +1,54 @@
+import serial
+import time
+
+ser=serial.Serial('/dev/serial0', 115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
+ser.write("AT+CGNSPWR=1\r")
+ser.write("AT+CGNSPWR?\r")
+
+print "Checking FONA808 powerstatus...."
+
+while True:
+	response = ser.readline()
+	print response
+	if "1" in response:
+		print("Power is on")
+		break
+
+ser.write("AT+CBC\r")
+
+print "Checking for battery levels...."
+
+while True:
+	response = ser.readline()
+	if "+CBC: 0" in response:
+		tempSplit = response.split(",")
+		batteryPercent = tempSplit[1]
+		voltageLevel = tempSplit[2]
+		print ("Battery percent = "+batteryPercent)
+		print ("Voltage level = "+voltageLevel)
+		break
+
+ser.write("AT+CGNSINF\r")
+
+print "Checking GPS coordinates...."
+
+while True:
+	response = ser.readline()
+	if "+CGNSINF: 1,1" in response:
+		tempSplit = response.split(",")
+		lat = tempSplit[3]
+		long = tempSplit[4]
+		print("Latitude = "+lat)
+		print("Longitude = "+long)
+		f=open("textfile.txt","w+")
+		f.write("\nLatitude: " +lat)
+		f.write("\nLongitude: " +long)
+		f.close()
+		break
+	if "+CGNSINF: 1,0" in response:
+		time.sleep(4)
+		ser.write("AT+CGNSINF\r")
+	else:
+		time.sleep(4)
+		ser.write("AT+CGNSINF\r")
+	
---
+++ startupTest.py
@@ -0,0 +1,42 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/serial0',115200,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,timeout=1)
+fail = 0
+ser.write("AT\r")
+while True:
+	response = ser.readline()
+	if "OK" in response:
+		break
+
+	else:
+		fail = fail +1
+		time.sleep(2)
+		ser.write("AT\r")
+
+	if  fail>5:
+		fail = 0
+		print "UART CONNECTION FAILED\n"
+		break
+
+ser.write("AT+CGNSINF\r")
+
+while True:
+	response = ser.readline()
+	if "+CGNSINF: 1,1" in response:
+		break
+
+	else:
+		fail = fail +1
+		time.sleep(2)
+		ser.write("AT+CGNSINF\r")
+
+	if fail>10:
+		print "CONNECTION TO SATALITE FAILED\n"
+		break
+
+ser.write("AT+CBC")
+
+while True: 
+	response = ser.readline()
+	if
