aa33036f32ec01b53c0e38ef266509eff74161ba
svan1616 <svan1616@student.ju.se>
2019-04-25 13:21:19 +0100

    Now uploads date and time if user is registered in firebase

--- main.py
+++ main.py
@@ -2,26 +2,24 @@ import bluetooth
 import time
 from datetime import date
 from datetime import datetime
-from databaseFb import db_compare
-from databaseFb import db_upload
+import databaseFb
 from threading import Thread
 import collections
+import re
 
 def bluetooth_scan():
-
-        maxunits =2
+        maxunits = 5
         timeoutm = 1
-        
 	devicelist = []
 	num = 0
 	lastadded = 0
 	atime = 0
 	devfound = False
 	while True:
-		#print("HEJ\n")
 		nrby = bluetooth.discover_devices(duration=2)
 		for item in nrby:
 			if not item in devicelist:
+				print(item)
 				devicelist.append(item)
 				num = num+1
 				devfound = True
@@ -31,29 +29,28 @@ def bluetooth_scan():
 		if devfound == False:
 			lastadded= atime
 
-
 		if num>=maxunits or abs(atime.minute-lastadded.minute)>=timeoutm:
 			for x in devicelist:
 				buffer.append(x)
 
 			devicelist = []
 
-
-
 def bluetooth_compareToUsers():
 	while True:
-		#print("da\n")
 		if len(buffer) is not 0:
 			value = buffer.popleft()
-			if not db_compare('/users',value):
-				db_upload('/users/'+value+'/pay_flag',datetime.now())
-
+			print(value)
+			if databaseFb.db_compare('/Users',value):
+				print("User found, uploading flag")
+				string = "/Users/{}".format(value)
+				date = datetime.now()
+				dateTime = re.findall(r"\d{4}.\d{2}.\d{2}.\d+.\d+","{}".format(date))
+				databaseFb.db_upload(string,'/pay_flag', "{}".format(dateTime[0]))
 		time.sleep(1)
 
 
-
-
 if __name__=="__main__":
+
 	buffer = collections.deque(maxlen=50)
 
 	t1 = Thread(target=bluetooth_scan)
